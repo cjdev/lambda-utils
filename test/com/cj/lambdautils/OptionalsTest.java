e89f10d3e8ca47dd2ef23b3f9108fb4d8b2c9f49
David Ron <dron@cj.com>
2015-10-13 14:05:23 -0700

    Adding an optionals.java that includes a little helper to filter down a stream of optionals to only inlude the stuff that exists.

---
+++ test/com/cj/lambdautils/OptionalsTest.java
@@ -0,0 +1,32 @@
+package com.cj.lambdautils;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+public class OptionalsTest {
+	
+	@Test
+	public void canFlattenAnOptional(){
+		//Given
+		Stream<Optional<Integer>> stream = Arrays.<Optional<Integer>>asList(Optional.empty(), Optional.of(1), Optional.of(2), Optional.empty(), Optional.empty(), Optional.of(3)).stream();
+		
+		//When
+		List<Integer> result = stream.flatMap(Optionals.nonEmpty()).collect(Collectors.toList());
+		
+		assertEquals(3, result.size());
+		assertEquals(1, result.get(0).intValue());
+		assertEquals(2, result.get(1).intValue());
+		assertEquals(3, result.get(2).intValue());
+		
+		
+		
+	}
+
+}
