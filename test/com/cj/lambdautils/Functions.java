8055cda9bf818f28e8f2086797dfd1ce9a3a9dcf
David Ron <dron@cj.com>
2015-12-01 15:54:26 -0800

    Typo Fix

--- test/com/cj/lambdautils/Functions.java
+++ test/com/cj/lambdautils/Functions.java
@@ -4,7 +4,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Functions {
-	public <T> Function<T, Void> voidFunction(Consumer<T> consumer){
+	public static <T> Function<T, Void> voidFunction(Consumer<T> consumer){
 		return (Function<T, Void>) t-> {
 			consumer.accept(t);
 			return null;
