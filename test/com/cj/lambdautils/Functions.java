1d70c220a509342e1a117e6d9af094c3ff05710e
David Ron <dron@cj.com>
2015-12-02 15:31:25 -0800

    Adding retypeValues which is something I have to do a lot.

--- test/com/cj/lambdautils/Functions.java
+++ test/com/cj/lambdautils/Functions.java
@@ -1,7 +1,9 @@
 package com.cj.lambdautils;
 
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Functions {
 	public static <T> Function<T, Void> voidFunction(Consumer<T> consumer){
@@ -9,5 +11,9 @@ public class Functions {
 			consumer.accept(t);
 			return null;
 		};
+	}
+	
+	public static <T, U, V> Map<V, U>  retypeValues(Map<V, T> input, Function<T, U> valueMapper){
+		return input.keySet().stream().collect(Collectors.toMap(k->k, k->valueMapper.apply(input.get(k))));
 	}
 }
