800dbbdfeb9e069ecc36f81517eeb9a6f2ebd945
David Ron <dron@cj.com>
2015-12-29 10:59:33 -0800

    Adding documentation

--- test/com/cj/lambdautils/Functions.java
+++ test/com/cj/lambdautils/Functions.java
@@ -9,6 +9,12 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class Functions {
+	
+	/**
+	 * Convenience method to convert a consumer into a "function" that returns void.
+	 * @param consumer
+	 * @return
+	 */
 	public static <T> Function<T, Void> voidFunction(Consumer<T> consumer){
 		return (Function<T, Void>) t-> {
 			consumer.accept(t);
@@ -16,6 +22,12 @@ public class Functions {
 		};
 	}
 	
+	/**
+	 * Convenience method to change the type of the values of a map.
+	 * @param input
+	 * @param valueMapper
+	 * @return
+	 */
 	public static <T, U, V> Map<V, U>  retypeValues(Map<V, T> input, Function<T, U> valueMapper){
 		return input.keySet().stream().collect(Collectors.toMap(k->k, v->{
 			try{
@@ -26,6 +38,21 @@ public class Functions {
 		}));
 	}
 	
+	/**
+	 * Converts the iterator to a non-parallel stream.
+	 * @param iterator
+	 * @return
+	 */
+	public static <T> Stream<T> streamOf(Iterator<T> iterator){
+		return streamOf(iterator, false);
+	}
+	
+	/**
+	 * Converts the iterator into a stream, can be parallel or not.
+	 * @param iterator
+	 * @param parallel
+	 * @return
+	 */
 	public static <T> Stream<T> streamOf(Iterator<T> iterator, boolean parallel){
 		Iterable<T> iterable = () -> iterator;
 		return StreamSupport.stream(iterable.spliterator(), parallel);
