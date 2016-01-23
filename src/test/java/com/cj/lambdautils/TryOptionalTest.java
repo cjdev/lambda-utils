14360e5d90cb8626b9ee1725af633afb16a7ffa5
David Ron <dron@cj.com>
2016-01-22 16:18:51 -0800

    Removing the deprecated functions in Try, bumping version.

--- src/test/java/com/cj/lambdautils/TryOptionalTest.java
+++ src/test/java/com/cj/lambdautils/TryOptionalTest.java
@@ -7,20 +7,6 @@ import java.util.Optional;
 import static org.junit.Assert.*;
 
 public class TryOptionalTest {
-    @Test
-    public void tryYieldsOptional() throws Exception {
-        assertEquals(Try.with(() -> 1 / 1), Optional.of(1));
-
-        assertTrue(Try.with(() -> new Object()).isPresent());
-
-        assertFalse(
-                Try.withThrowing((ThrowingSupplier<Integer>) () -> {
-                    throw new Exception("boom");
-                }).isPresent());
-
-        assertFalse(Try.withThrowing(() -> 1 / 0).isPresent());
-    }
-    
     @Test(expected=RuntimeException.class)
     public void canWrapExceptionsFromAThrowingRunnable(){
     	Try.to(()->throwCheckedException());
